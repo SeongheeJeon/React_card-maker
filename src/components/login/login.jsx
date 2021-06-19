d2b666ab3c321152e5428c46c5cf46b3b788a5cd
DESKTOP-76V6K1K\se5ng <c9604100@gmail.com>
2021-06-19 18:55:43 +0900

    10.4~10.5 - Add login and logout functionality

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../header/header";
 import Footer from "../footer/footer";
 import styles from "./login.module.css";
+import { useHistory } from "react-router-dom";
 
 const Login = ({ authService }) => {
+  const history = useHistory();
+  const goToMaker = (userId) => {
+    history.push({
+      pathname: "/maker",
+      state: { id: userId },
+    });
+  };
+
   const onLogin = (event) => {
     authService //
       .login(event.currentTarget.textContent)
-      .then(console.log);
+      .then((data) => goToMaker(data.user.uid));
   };
+
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      user && goToMaker(user.uid);
+    });
+  });
+
   return (
     <section className={styles.login}>
-      <Header />
+      <Header onLogout={null} />
       <section>
         <h1>Login</h1>
         <ul className={styles.list}>
